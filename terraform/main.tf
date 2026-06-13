e436e4ded52f77f42c990ddcc4a126c0ec7c8c11
ABHISHEK-CLOUD1 <abhishek010601@gmail.com>
2026-06-13 22:26:37 +0000

    Isolate deployment scope to cloud assets and pivot resources to AD-2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,221 +1,18 @@
 terraform {
   required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.46.0"
-    }
+    proxmox = { source = "bpg/proxmox"; version = "0.46.0" }
+    oci     = { source = "oracle/oci"; version = "~> 5.0" }
   }
 }
 
+# Empty local provider skeleton to satisfy dependency structures
 provider "proxmox" {
   endpoint  = "https://192.168.155.128:8006/"
   api_token = "root@pam!terraform=bff2d81c-dce2-4fe5-ad89-66800425ec59"
   insecure  = true
 }
 
-resource "proxmox_virtual_environment_container" "test_container" {
-  node_name = "abhi"
-  vm_id     = 101
-  lifecycle { ignore_changes = all }
-  initialization {
-    hostname = "terraform-test"
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-  operating_system {
-    template_file_id = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
-    type             = "ubuntu"
-  }
-  cpu    { cores = 1 }
-  memory { dedicated = 512 }
-  disk {
-    datastore_id = "local-lvm"
-    size         = 8
-  }
-  network_interface {
-    name   = "eth0"
-    bridge = "vmbr0"
-  }
-  started = true
-}
-
-resource "proxmox_virtual_environment_container" "jenkins" {
-  node_name = "abhi"
-  vm_id     = 102
-  lifecycle { ignore_changes = all }
-  initialization {
-    hostname = "jenkins"
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-  operating_system {
-    template_file_id = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
-    type             = "ubuntu"
-  }
-  cpu    { cores = 2 }
-  memory { dedicated = 1024 }
-  disk {
-    datastore_id = "local-lvm"
-    size         = 10
-  }
-  network_interface {
-    name   = "eth0"
-    bridge = "vmbr0"
-  }
-  started = true
-}
-
-resource "proxmox_virtual_environment_container" "staging" {
-  node_name = "abhi"
-  vm_id     = 103
-  lifecycle { ignore_changes = all }
-  initialization {
-    hostname = "staging"
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-  operating_system {
-    template_file_id = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
-    type             = "ubuntu"
-  }
-  cpu    { cores = 1 }
-  memory { dedicated = 512 }
-  disk {
-    datastore_id = "local-lvm"
-    size         = 8
-  }
-  network_interface {
-    name   = "eth0"
-    bridge = "vmbr0"
-  }
-  started = true
-}
-
-resource "proxmox_virtual_environment_container" "production" {
-  node_name = "abhi"
-  vm_id     = 104
-  lifecycle { ignore_changes = all }
-  initialization {
-    hostname = "production"
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-  operating_system {
-    template_file_id = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
-    type             = "ubuntu"
-  }
-  cpu    { cores = 1 }
-  memory { dedicated = 512 }
-  disk {
-    datastore_id = "local-lvm"
-    size         = 8
-  }
-  network_interface {
-    name   = "eth0"
-    bridge = "vmbr0"
-  }
-  started = true
-}
-
-resource "proxmox_virtual_environment_container" "samba_dc" {
-  node_name = "abhi"
-  vm_id     = 105
-  lifecycle { ignore_changes = all }
-  initialization {
-    hostname = "samba-dc"
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-  operating_system {
-    template_file_id = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
-    type             = "ubuntu"
-  }
-  cpu    { cores = 1 }
-  memory { dedicated = 512 }
-  disk {
-    datastore_id = "local-lvm"
-    size         = 10
-  }
-  network_interface {
-    name   = "eth0"
-    bridge = "vmbr0"
-  }
-  started = true
-}
-
-resource "proxmox_virtual_environment_container" "prometheus" {
-  node_name = "abhi"
-  vm_id     = 106
-  lifecycle { ignore_changes = all }
-  initialization {
-    hostname = "prometheus"
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-  operating_system {
-    template_file_id = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
-    type             = "ubuntu"
-  }
-  cpu    { cores = 1 }
-  memory { dedicated = 512 }
-  disk {
-    datastore_id = "local-lvm"
-    size         = 8
-  }
-  network_interface {
-    name   = "eth0"
-    bridge = "vmbr0"
-  }
-  started = true
-}
-
-resource "proxmox_virtual_environment_container" "grafana" {
-  node_name = "abhi"
-  vm_id     = 107
- lifecycle { ignore_changes = all } 
- initialization {
-    hostname = "grafana"
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-  operating_system {
-    template_file_id = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
-    type             = "ubuntu"
-  }
-  cpu    { cores = 2 }
-  memory { dedicated = 512 }
-  disk {
-    datastore_id = "local-lvm"
-    size         = 8
-  }
-  network_interface {
-    name   = "eth0"
-    bridge = "vmbr0"
-  }
-  started = true
-}
-
+# Core OCI Provider configuration
 provider "oci" {
   tenancy_ocid     = var.oci_tenancy_ocid
   user_ocid        = var.oci_user_ocid
@@ -224,8 +21,11 @@ provider "oci" {
   region           = var.oci_region
 }
 
+# ===================================================================
+# CLOUD VM 1: Grafana Management Host (1 OCPU, 6 GB RAM)
+# ===================================================================
 resource "oci_core_instance" "cloud_vm_1_grafana" {
-  availability_domain = "ZjLz:AP-MUMBAI-1-AD-1" 
+  availability_domain = "ZjLz:AP-MUMBAI-1-AD-2" # Switched to AD-2
   compartment_id      = var.oci_tenancy_ocid
   display_name        = "cloud-vm-1-grafana"
   shape               = "VM.Standard.A1.Flex"
@@ -251,8 +51,11 @@ resource "oci_core_instance" "cloud_vm_1_grafana" {
   }
 }
 
+# ===================================================================
+# CLOUD VM 2: Kubernetes Node Host (3 OCPUs, 18 GB RAM)
+# ===================================================================
 resource "oci_core_instance" "cloud_vm_2_kubernetes" {
-  availability_domain = "ZjLz:AP-MUMBAI-1-AD-1" 
+  availability_domain = "ZjLz:AP-MUMBAI-1-AD-2" # Switched to AD-2
   compartment_id      = var.oci_tenancy_ocid
   display_name        = "cloud-vm-2-kubernetes"
   shape               = "VM.Standard.A1.Flex"
